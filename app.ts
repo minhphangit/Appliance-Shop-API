8f5bd258d2dbae787d25497278e1e4a424c2c2a9
Minh Phan <133383070+minhphangit@users.noreply.github.com>
2024-06-13 11:41:50 +0700

    Update app.ts1

--- app.ts
+++ app.ts
@@ -20,25 +20,22 @@ const file = fs.readFileSync(path.resolve('./openapi.yaml'), 'utf8');
 const swaggerDocument = YAML.parse(file);
 
 //MongoDB connection with initial retry
-const mongooseConnection = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_URL}`;
+const mongooseConnection = 'mongodb://' + process.env.MONGODB_URL;
 mongoose.connection.on('connected', () => console.log('Connected to ' + mongooseConnection));
 mongoose.connection.on('disconnected', () => console.log('MongoDB lost connection'));
 mongoose.connection.on('error', (error) => console.error('Error in MongoDb connection: ' + error));
-
 const MongoDBConnect = () => {
   mongoose
     .connect(mongooseConnection, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
+      user: process.env.MONGODB_USER,
+      pass: process.env.MONGODB_PASS,
+      serverSelectionTimeoutMS: 5000,
     })
     .catch((err) => {
-      console.error('««««« err »»»»»', err);
       console.log('MongoDB connection unsuccessful, retry after 5 seconds.');
       setTimeout(MongoDBConnect, 5000);
     });
 };
-
 MongoDBConnect();
 
 const app: Express = express();
