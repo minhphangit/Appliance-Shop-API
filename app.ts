06728feb67293a2784e5a25587fd44852e34a2f9
minhcopilot <21115053120333@sv.ute.udn.vn>
2024-06-13 16:52:22 +0700

    update cors 3

--- app.ts
+++ app.ts
@@ -61,25 +61,9 @@ AppDataSource.initialize().then(async () => {
     }),
   );
 
-  const allowedOrigins = [
-    'https://appliance-shop-admin-blue.vercel.app/',
-    'https://appliance-shop-admin-blue.vercel.app/*',
-    'http://localhost:4000/',
-    'http://localhost:3000/',
-  ];
-
   app.use(
     cors({
-      origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
-      credentials: true,
+      origin: '*',
     }),
   );
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
